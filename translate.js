9062c7061cb3f7f0cf3ee517cbe8b3144720e95d
Mike Haggerty <ghoti143@gmail.com>
2018-04-12 23:24:35 -0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,5 +1,21 @@
+var EOSNY_en_zh = {
+  'EOS New York': 'EOS纽约',
+  'About Us': '关于我们',
+  'Our Values': '我们的价值',
+  'Events': '活动',
+  'Vote For Us': '为美国投票',
+  'Connect With Us': '联系我们',
+  'Our Roadmap': '我们的路线图',
+  'Block Producer Specs': '阻止制片人规格',
+  'Our Initiatives': '我们的举措',
+  'Our Team': '我们的队伍',
+  'Our Advisors': '我们的顾问'
+}  
+
 function toZH() {
-  $( "h1" ).each(function( index ) {
-    $(this).text("时候")
+  $( "h1" ).each(function() {
+    var en = $(this).text();
+    var zh = ENSNY_en_zh[en];
+    $(this).text(zh);
   });
 }
