2f7d3b93bc77f23ced421f825153bae5aff2dc45
Mike Haggerty <ghoti143@gmail.com>
2018-04-13 00:20:33 -0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -19,7 +19,18 @@ var EOSNY_en_zh = {
   'Connect': '连',
   'Plan': '计划',
   'Initiatives': '倡议',
-  'Team': '球队'
+  'Team': '球队',
+  //h3
+  'Always Adding Value': '始终增加价值',
+  'Everything a Block Away': '一切皆有可能',
+  'Subscribe to our mailing list': '订阅我们的邮件列表',
+  'The EOS Resource Planner or ERP': 'EOS资源计划员或ERP',
+  'Kevin Rose, Co-Founder, Head of Community': '社区负责人联合创始人Kevin Rose',
+  'Rick Schlesinger, Co-Founder, Head of Strategy': 'Rick Schlesinger，联合创始人，战略主管',
+  'Buddy Deck, Head of Block Production': '块生产主管Buddy Deck',
+  'Mike Haggerty, Head of dApp Development': 'dApp Development负责人Mike Haggerty',
+  'Joshua Hecht, Advisor': 'Joshua Hecht，顾问',
+  'Liya Palagashvili, Ph.D., Advisor': 'Liya Palagashvili，博士，顾问',
 }  
 
 function toZH() {
@@ -36,6 +47,13 @@ function toZH() {
     $(this).data('orig', en);
     $(this).text(zh);
   });
+  
+  $( "h3" ).each(function() {
+    var en = $(this).text().trim();
+    var zh = EOSNY_en_zh[en];
+    $(this).data('orig', en);
+    $(this).text(zh);
+  });
 }
 
 function toEN() {
@@ -45,6 +63,11 @@ function toEN() {
   });
   
   $( "nav a" ).each(function() {
+    var en = $(this).data('orig');
+    $(this).text(en);
+  });
+  
+  $( "h3" ).each(function() {
     var en = $(this).data('orig');
     $(this).text(en);
   });
